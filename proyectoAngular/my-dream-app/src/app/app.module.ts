61eb42cf86f869fc6dfbbf65d272c64c4d5a1a2e
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-08 23:47:00 -0500

    Solucionando la imagen duplicada de App Component quitándolo de las rutas creadas, ya pertenece a la ruta raíz

--- proyectoAngular/my-dream-app/src/app/app.module.ts
+++ proyectoAngular/my-dream-app/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Route } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Route[] =[
-    { path: '', component: AppComponent },
     { path: 'hello', component: HelloWorldComponent },
     { path: 'about', component: AboutComponent }
 ];
--- proyectoAngular/my-dream-app/src/app/app.routes.ts
+++ proyectoAngular/my-dream-app/src/app/app.routes.ts
@@ -1,10 +1,8 @@
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    { path: '', component: AppComponent },
     { path: 'hello', component: HelloWorldComponent },
     { path: 'about', component: AboutComponent }
 ];
