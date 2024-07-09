81c4bdad082eedece73cd5680298f7084414ebb3
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-08 23:39:00 -0500

    Angular 2: Haciendo el DataBinding con datos de formulario, los componentes no se declaran por ser standalone

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,58 +1,22 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserComponent } from './user/user.component' 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HelloWorldComponent, UserComponent],
+  imports: [CommonModule,
+            HelloWorldComponent,
+            UserComponent,
+            FormsModule
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  @ViewChild('newUser') newUserInput!: ElementRef;
-
-  users = ['ryan', 'joe', 'cameron', 'john', 'jhonatan', 'rodolfo'];
-  activated = false;
-  title = 'my-dream-app';
-	name : string;
-	email;
-	webpage : string;
-	hobbies : string[];
-  showHobbies : boolean;
-
-  constructor() {
-    console.log("Constructor working...");
-    this.name = "Jhonatan Benjamin Mamani Céspedes";
-    this.email = "jmamanices@unsa.edu.pe";
-    this.webpage = "http://www.unsa.edu.pe";
-    this.hobbies = ["Futbol","Programacion","Leer","Editar y modelar (3D)"];
-    this.showHobbies = false;
-  }
-  toggleHobbies() {
-		this.showHobbies = !this.showHobbies;
-	}
-  newHobby(hobby:any) {
-		this.hobbies.push(hobby.value);
-		hobby.value = "";
-		return false;
-	}
-  sayHello() {
-		alert("Hola desde app.component");
-	}
-  addUser(newUser: any){
-		this.users.push(newUser.value);
-    newUser.value = '';
-    this.newUserInput.nativeElement.focus();
-    return false;
-	}
-  deleteUser(user:any) {
-		for(let i = 0; i < this.users.length; i++){
-			if(user == this.users[i]){
-				this.users.splice(i, 1);
-			}
-		}
-	}
+  name : string = "Jhonatan B. Mamani Céspedes";
+  age : number = 18;
 }
---
+++ proyectoAngular/my-dream-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+    ],
+    providers: [],
+})
+export class AppModule { }
