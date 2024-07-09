0af04b7ff1d6265f23a952cfc23a81d664fff89d
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-07-08 23:27:00 -0500

    Generando un Componente hello-world e importándolo en app.component.ts

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HelloWorldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
