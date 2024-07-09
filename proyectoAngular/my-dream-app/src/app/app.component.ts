883c3ecfe0cb7a0572d0956861b0fc8a1818ef93
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-07-08 22:33:15 -0500

    Agregando Constructor y usando For e Ifs (aún no se usa la función showhobbies)

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-dream-app';
-	name = "Jhonatan Benjamin Mamani Céspedes";
-	email = "jmamanices@unsa.edu.pe";
-	webpage = "http://www.unsa.edu.pe";
+	name : string;
+	email;
+	webpage : string;
+	hobbies : string[];
+
+  constructor() {
+    console.log("Constructor working...");
+    this.name = "Jhonatan Benjamin Mamani Céspedes";
+    this.email = "jmamanices@unsa.edu.pe";
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Futbol","Programacion","Leer","Editar y modelar (3D)"];
+  }
 }
