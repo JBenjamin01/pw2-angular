37e50563e077f81f4ecf5e9cb16ad6c5b48a9453
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-08 23:52:00 -0500

    Preparando proyecto para hacer deploy

---
+++ proyectoAngular/my-dream-app/dist/my-dream-app/polyfills.02ae3c83f85fe03f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_dream_app=self.webpackChunkmy_dream_app||[]).push([[461],{50:()=>{}},a=>{a(a.s=50)}]);
--- proyectoAngular/my-dream-app/dist/my-dream-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-N3LHNFDV.mjs";import"./chunk-S7G7LHKJ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
