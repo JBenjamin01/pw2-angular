a3b7f2f0f29b60f9762f226b5991a776f3701bcd
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-07-08 23:29:00 -0500

    Pasando datos al Componente user importado en app.component.ts

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserComponent } from './user/user.component' 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HelloWorldComponent],
+  imports: [CommonModule, HelloWorldComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  users = ['ryan', 'joe', 'cameron', 'john', 'jhonatan', 'rodolfo'];
+  activated = false;
   title = 'my-dream-app';
 	name : string;
 	email;
